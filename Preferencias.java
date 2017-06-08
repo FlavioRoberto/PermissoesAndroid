2be4fe01bf50622296ff49b30f2f61f975809e0c
FlavioRoberto <flaviolive3@hotmail.com>
2017-06-08 12:22:27 -0300

    Create Preferencias.java
    Classe para armazenar um login de usuario usando SharedPrefereces

---
+++ Preferencias.java
@@ -0,0 +1,48 @@
+package com.cursoandroid.cursoandroid.admin.exemploPreferencia.helper;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+import java.util.HashMap;
+
+/**
+ * Created by Flavio on 07/06/2017.
+ */
+
+public class Preferencias {
+
+    private Context context;
+    private SharedPreferences preferences;
+    private final String NOME_ARQUIVO = "preferenciawhatsapp";
+    private final int MODE = 0;
+    private SharedPreferences.Editor editor;
+    private final String CHAVE_NOME="nome";
+    private final String CHAVE_TELEFONE="telefone";
+    private final String CHAVE_TOKEN="token";
+
+    public  Preferencias(Context contextoParametro){
+        context = contextoParametro;
+        preferences = context.getSharedPreferences(NOME_ARQUIVO,MODE);
+        editor = preferences.edit();
+    }
+
+    public void salvarusuarioPreferences(String nome, String telefone, String token){
+        editor.putString(CHAVE_NOME,nome);
+        editor.putString(CHAVE_TELEFONE,telefone);
+        editor.putString(CHAVE_TOKEN,token);
+        editor.commit();
+
+    }
+
+    //Hasgmap retorna uma lista com insices
+    public HashMap<String,String> retornaDadosusuario(){
+
+        HashMap<String,String> dados = new HashMap<>();
+        dados.put(CHAVE_NOME,preferences.getString(CHAVE_NOME,null));
+        dados.put(CHAVE_TOKEN,preferences.getString(CHAVE_TOKEN,null));
+        dados.put(CHAVE_TELEFONE,preferences.getString(CHAVE_TELEFONE,null));
+
+        return dados;
+
+    }
+}
